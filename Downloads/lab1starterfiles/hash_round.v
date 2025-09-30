6acffe3b768f26b9186fa146327a4f031db5aa27
sofiabaroudi <sbaroudi@stanford.edu>
2025-09-30 09:32:09 -0700

    Update hash_round.v

--- Downloads/lab1starterfiles/hash_round.v
+++ Downloads/lab1starterfiles/hash_round.v
@@ -1,15 +1,40 @@
-module hash_round (
-   
-);
-   
-	// Declarations
+module hash_round #(
+  parameter ROUND = 0
+)(
+  input  wire [7:0]  in_byte,
+  input  wire [31:0] in_state,
+  output wire [31:0] out_state
+);   
+  // Declarations
+  wire [7:0] a, b, c, d;
+  reg  [7:0] mix;
+  wire [7:0] mixed_a;
+  wire [7:0] rotated_mixed_a;	
 	
-	// State splitting
+  // State splitting
+  assign {d, c, b, a} = in_state;
 	
-	// Mix function
+  // Mix function
+  always @(*) begin
+    if (ROUND <= 2)
+      mix = (d & c) | (~d & b);
+    else if (ROUND <= 4)
+      mix = d ^ c ^ b;
+    else
+      mix = (d & b) | (c & ~b);
+  end	
+
+  assign mixed_a = a + in_byte + mix;
 	
-	// Rotator
+  // Rotator
+  rotator #(.WIDTH(8)) rot_inst (
+    .in       (mixed_a),
+    .out      (rotated_mixed_a),
+    .distance (ROUND),       
+    .direction(1'b1)       
+  );
 	
-	// Output state assignment
+  // Output state assignment
+  assign out_state = {c, b, d, rotated_mixed_a};
 
 endmodule
