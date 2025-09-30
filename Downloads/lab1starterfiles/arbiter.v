38f4a0ca859c38a1b3949cf2396ab3a356854a01
rudraksh-art <rudraksh@stanford.edu>
2025-09-30 16:36:08 -0700

    Update arbiter.v
    Updated

--- Downloads/lab1starterfiles/arbiter.v
+++ Downloads/lab1starterfiles/arbiter.v
@@ -1,21 +1,24 @@
 module arbiter (
-    input wire[7:0] in,
-    output reg[7:0] out
+        input [7:0] in,
+        output reg [7:0] out
+   
 );
-    always @ (*) begin
-            casex (in)
-                8'b00000000: out = 8'b00000000;
-                8'bxxxxxxx1: out = 8'b00000001;
-                8'bxxxxxx1x: out = 8'b00000010;
-                8'bxxxxx1xx: out = 8'b00000100;
-                8'bxxxx1xxx: out = 8'b00001000;
-                8'bxxx1xxxx: out = 8'b00010000;
-                8'bxx1xxxxx: out = 8'b00100000;
-                8'bx1xxxxxx: out = 8'b01000000;
-                8'b1xxxxxxx: out = 8'b10000000;
-                
-                default: out = 8'b00000000;//case c
-            endcase
+    always @(*) begin
+        casex (in)
+            8'bxxxxxxx1: out = 8'b00000001;
+            8'bxxxxxx10: out = 8'b00000010;
+            8'bxxxxx100: out = 8'b00000100;
+            8'bxxxx1000: out = 8'b00001000;
+            8'bxxx10000: out = 8'b00010000;
+            8'bxx100000: out = 8'b00100000;
+            8'bx1000000: out = 8'b01000000;
+            8'b10000000: out = 8'b10000000;
             
-     end
+            default: out = 8'b00000000;
+            
+        endcase
+        
+    end
+	// module body goes here
+
 endmodule
