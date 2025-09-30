6e2fcd39bcdf41c185f8f501e49f82d55212e5e9
Tito004 <cha4@stanford.edu>
2025-09-30 16:24:57 -0700

    Update length_finder_tb.v

--- Downloads/lab1starterfiles/length_finder_tb.v
+++ Downloads/lab1starterfiles/length_finder_tb.v
@@ -1,6 +1,7 @@
 module length_finder_tb ();
 
    reg [63:0] in;
+   reg [3:0] expected;
    wire [3:0] len;
    
    length_finder dut (
@@ -12,48 +13,49 @@ module length_finder_tb ();
       in = 64'hAABBCCDDEEFFAA00;
       expected = 4'd0;
       #5
-      $display("in=%h -> out=%d, expected=%d", in, out, expected);
+      $display("in=%h -> out=%d, expected=%d", in, len, expected);
       
       in = 64'hAABBCCDDEEFFAA99;
       expected = 4'd8;
       #5
-      $display("in=%h -> out=%d, expected=%d", in, out, expected);
+      $display("in=%h -> out=%d, expected=%d", in, len, expected);
       
       in = 64'hAABBCCDDEEFF00AA;
       expected = 4'd1;
       #5
-      $display("in=%h -> out=%d, expected=%d", in, out, expected);
+      $display("in=%h -> out=%d, expected=%d", in, len, expected);
       
       in = 64'hAABBCCDDEE00FFAA;
       expected = 4'd2;
       #5
-      $display("in=%h -> out=%d, expected=%d", in, out, expected);
+      $display("in=%h -> out=%d, expected=%d", in, len, expected);
       
       in = 64'hAABBCC00EE00FFAA;
       expected = 4'd2;
       #5
-      $display("in=%h -> out=%d, expected=%d", in, out, expected);
+      $display("in=%h -> out=%d, expected=%d", in, len, expected);
       
       in = 64'h00BBCCDDEE44FFAA;
       expected = 4'd7;
       #5
-      $display("in=%h -> out=%d, expected=%d", in, out, expected);
+      $display("in=%h -> out=%d, expected=%d", in, len, expected);
       
       in = 64'h00BBCC00EE44FFAA;
-      expected = 4'd3;
-      #5
-      $display("in=%h -> out=%d, expected=%d", in, out, expected);
-      
-      in = 64'h44BBC00DEE44FFAA;
       expected = 4'd4;
       #5
-      $display("in=%h -> out=%d, expected=%d", in, out, expected);
+      $display("in=%h -> out=%d, expected=%d", in, len, expected);
+      
+      in = 64'h44BBC00DEE44FFAA;
+      expected = 4'd8;
+      #5
+      $display("in=%h -> out=%d, expected=%d", in, len, expected);
       
       in = 0;
       expected = 4'd0;
       #5
-      $display("in=%h -> out=%d, expected=%d", in, out, expected);
+      $display("in=%h -> out=%d, expected=%d", in, len, expected);
       
    end
    
 endmodule
+
