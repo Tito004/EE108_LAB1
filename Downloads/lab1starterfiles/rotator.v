97924798576cd26403cfbc9984a06520464ac212
Tito004 <cha4@stanford.edu>
2025-09-29 18:52:11 -0700

    rotator.v changes

--- Downloads/lab1starterfiles/rotator.v
+++ Downloads/lab1starterfiles/rotator.v
@@ -1,11 +1,26 @@
-module rotator #(/* parameter list goes here */) (
-   
+module rotator #(parameter WIDTH = 8) (
+    input wire[WIDTH-1:0] in,
+    output wire[WIDTH-1:0] out,
+    input wire[7:0] distance,
+    input wire direction  
 );
 
 	// Declarations
+	wire [2*WIDTH-1:0] double_in;
+	wire [2*WIDTH-1:0] double_left;
+	wire [2*WIDTH-1:0] double_right;
+	wire [WIDTH-1:0] left;
+	wire [WIDTH-1:0] right;
 	
 	// Rotating logic
+	assign double_in = {in, in};
+	assign double_left = double_in << distance;
+	assign double_right = double_in >> distance;
+	assign left = double_left[2*WIDTH-1:WIDTH];
+	assign right = double_right[WIDTH-1:0];
+	
 	
 	// Output mux logic
+	assign out = direction ? left : right;
 	
 endmodule
