e93b3ac3332b0fee8ac00da5b71370fe0d1f6a7d
Tito004 <cha4@stanford.edu>
2025-09-30 16:17:06 -0700

    Update length_finder.v

--- Downloads/lab1starterfiles/length_finder.v
+++ Downloads/lab1starterfiles/length_finder.v
@@ -1,13 +1,13 @@
 module length_finder (
-   input wire [63:0] string,
-   output wire [3:0] length
+    input wire [63:0] string,
+    output wire [3:0] length
 );
-   
-	// wire declarations
+
+    // wire declarations
     wire [7:0] is_character_null;
     wire [7:0] arbiter_out;
-	
-	// is_character_null logic (8 lines of it!)
+
+    // is_character_null logic (8 lines of it!)
     assign is_character_null[0] = ~|string[7:0];
     assign is_character_null[1] = ~|string[15:8];
     assign is_character_null[2] = ~|string[23:16];
@@ -16,11 +16,11 @@ module length_finder (
     assign is_character_null[5] = ~|string[47:40];
     assign is_character_null[6] = ~|string[55:48];
     assign is_character_null[7] = ~|string[63:56];
-	
-	// arbiter
+
+    // arbiter
     arbiter arbiter_inst (
-        .req(is_character_null),
-	   .gnt(arbiter_out)
+        .in(is_character_null),
+        .out(arbiter_out)
     );
 
     // encoder
@@ -28,7 +28,8 @@ module length_finder (
         .in(arbiter_out),
         .out(length[2:0])
     );
-    
+
     assign length[3] = (is_character_null == 8'b00000000);
-  
+
 endmodule
+
